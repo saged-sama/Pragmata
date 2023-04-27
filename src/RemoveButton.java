eaf879cf18175bdad7ed0eeb9480eb9cb3a03c66
Emon <schrodingersrats@gmail.com>
2023-04-27 12:05:53 +0600

    Update RemoveButton.java

--- src/RemoveButton.java
+++ src/RemoveButton.java
@@ -1,7 +1,7 @@
 import javafx.scene.control.Button;
 
 public class RemoveButton extends Button{
-    String style = "-fx-background-color: #FF4F84; " +
+    String style = "-fx-background-color: #FF0000; " +
         "-fx-text-fill: #FFFFFF; " +
         "-fx-font-size: 14px; " +
         "-fx-padding: 10px 20px";
